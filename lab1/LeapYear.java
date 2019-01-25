f60c568dbcd15f23e2eee0e1f958fce936542b45
David Lu <davidlu@berkeley.edu>
2019-01-25 13:26:44 -0800

    finished leapyear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,5 +1,5 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author David Lu
  */
 public class LeapYear {
 
@@ -28,6 +28,13 @@ public class LeapYear {
                 System.out.printf("%s is not a valid number.\n", args[i]);
             }
         }
+    }
+    /** returns whether year is a leap year or not. */
+    public static boolean isLeapYear(int year){
+        if(year%400==0 || (year%4==0 && year%100!=0)){
+            return true;
+        }
+        return false;
     }
 }
 
