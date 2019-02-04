e68ca1824c3385eb8e10c3016befd705d975ca4c
David Lu <davidlu@berkeley.edu>
2019-02-03 22:03:05 -0800

    addLast and addFirst methods. dont know if they work

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,18 +1,45 @@
-public class LinkedListDeque<val> {
-    
+public class LinkedListDeque<val>{
+    Node tail;
+    Node head;
+    Node sentinel;
+    int size;
     /**Node class with prev, next, and generic item attribute*/
-    public class Node{
+    public class Node<val>{
         Node prev;
         Node next;
         val item;
-        public Node(Node p, Node n, val i){
-            this.prev = p;
-            this.next = n;
+        public Node(val i){
             this.item = i;
         }
     }
 
-    public LinkedListDeque(val v){
+    public LinkedListDeque(){
+        sentinel = new Node(null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+        tail = sentinel;
+        head = sentinel;
+    }
+    public void addFirst(val item){
+        Node temp = sentinel.next;
+        Node new_node = new Node(item);
+        sentinel.next = new_node;
+        new_node.prev = sentinel;
+        new_node.next = temp;
+        temp.prev = new_node;
+        size += 1;
+    }
+    public void addLast(val item){
+        Node temp = sentinel.prev;
+        Node new_node = new Node(item);
+        new_node.next = temp;
+        new_node.prev = temp.prev;
+        temp.prev = new_node;
+        temp.prev.next = new_node;
+        size += 1;
+    }
 
+    public int size(){
+        return size;
     }
 }
