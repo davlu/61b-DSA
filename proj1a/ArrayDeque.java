4bc24d48042b0dc88ef48391242794550d2e0acb
David Lu <davidlu@berkeley.edu>
2019-02-08 00:45:36 -0800

    F style

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -136,5 +136,4 @@ public class ArrayDeque<T> {
         }
         return this.items[temp];
     }
-    
 }
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -113,5 +113,4 @@ public class LinkedListDeque<T> {
         }
         System.out.println(deque);
     }
-
 }
