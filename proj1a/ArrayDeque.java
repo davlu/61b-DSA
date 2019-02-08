7af0a70f6511a54dbe8ef9244706c14f21798a10
David Lu <davidlu@berkeley.edu>
2019-02-07 23:24:39 -0800

    LAST COMMIT???

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,129 +1,132 @@
 public class ArrayDeque<T> {
+
     private int size = 8;
-    private int array_Size = 0;
-    private T [] items;
+    private int arraySize = 0;
+    private T[] items;
     private int firstPointer = 4;
     private int lastPointer = 5;
+
     public ArrayDeque() {
-        this.items = (T []) new Object[8];
+        this.items = (T[]) new Object[8];
     }
 
     public ArrayDeque(ArrayDeque other) {
-        this.items = (T []) new Object[other.size];
+        this.items = (T[]) new Object[other.size];
         for (int i = 0; i < other.size; i++) {
             this.items[i] = (T) other.items[i];
         }
         this.firstPointer = other.firstPointer;
         this.lastPointer = other.lastPointer;
-        this.array_Size = other.array_Size;
+        this.arraySize = other.arraySize;
         this.size = other.size;
     }
+
     public void addFirst(T item) {
-        if(this.array_Size == this.size) {
-            resizeBig(this.array_Size*2);
+        if (this.arraySize == this.size) {
+            resizeBig(this.arraySize * 2);
         }
         this.items[this.firstPointer] = item;
-        if(this.firstPointer == 0) {
-            this.firstPointer = this.size-1;
-        }
-        else {
+        if (this.firstPointer == 0) {
+            this.firstPointer = this.size - 1;
+        } else {
             this.firstPointer--;
         }
-        this.array_Size++;
+        this.arraySize++;
     }
+
     public void addLast(T item) {
-        if (this.array_Size == this.size) {
-            resizeBig(this.array_Size*2);
+        if (this.arraySize == this.size) {
+            resizeBig(this.arraySize * 2);
+            this.items[this.lastPointer] = item;
+        } else {
             this.items[this.lastPointer] = item;
         }
-        else {
-            this.items[this.lastPointer] = item;
-        }
-        this.lastPointer = (this.lastPointer+1) % this.size;
-        this.array_Size++;
+        this.lastPointer = (this.lastPointer + 1) % this.size;
+        this.arraySize++;
     }
 
     public boolean isEmpty() {
-        return this.array_Size == 0;
+        return this.arraySize == 0;
     }
+
     public int size() {
-        return this.array_Size;
+        return this.arraySize;
     }
+
     public void printDeque() {
         String deque = "";
         int start = this.firstPointer + 1;
         for (int i = 0; i < this.size; i++) {
             deque = deque + " " + this.items[start];
-            if (start == this.size - 1){
+            if (start == this.size - 1) {
                 start = 0;
-            }
-            else {
+            } else {
                 start += 1;
             }
         }
         System.out.println(deque);
     }
+
     public T removeFirst() {
-        if (this.size > 16 && (double) this.array_Size / this.size < 0.25) {
-            resizeSmall(this.array_Size * 4);
+        if (this.size > 16 && (double) this.arraySize / this.size < 0.25) {
+            resizeSmall(this.arraySize * 4);
         }
         this.firstPointer = (this.firstPointer + 1) % this.size;
         T holder = this.items[this.firstPointer];
         this.items[this.firstPointer] = null;
-        if (array_Size == 0) {
+        if (arraySize == 0) {
             return holder;
         }
-        this.array_Size--;
+        this.arraySize--;
         return holder;
     }
+
     public T removeLast() {
-        if(this.size > 16 && (double) this.array_Size / this.size < 0.25) {
-            resizeSmall(this.array_Size * 4);
+        if (this.size > 16 && (double) this.arraySize / this.size < 0.25) {
+            resizeSmall(this.arraySize * 4);
         }
         this.lastPointer = Math.floorMod(this.lastPointer - 1, this.size);
         T holder = this.items[this.lastPointer];
         this.items[this.lastPointer] = null;
-        if(this.array_Size == 0) {
+        if (this.arraySize == 0) {
             return holder;
         }
-        this.array_Size--;
+        this.arraySize--;
         return holder;
     }
 
-    private void resizeBig(int new_Size) {
-        T[] resized_Items = (T []) new Object[new_Size];
+    private void resizeBig(int newSize) {
+        T[] resizedItems = (T[]) new Object[newSize];
         int start = (this.firstPointer + 1) % this.size;
-        for (int i = 0; i < this.size; i++){
-            resized_Items[i] = this.items[start];
-            if(start == this.size - 1){
+        for (int i = 0; i < this.size; i++) {
+            resizedItems[i] = this.items[start];
+            if (start == this.size - 1) {
                 start = 0;
-            }
-            else{
+            } else {
                 start += 1;
             }
         }
-        this.size = new_Size;
-        this.items = resized_Items;
+        this.size = newSize;
+        this.items = resizedItems;
         this.firstPointer = this.size - 1;
-        this.lastPointer = this.array_Size;
+        this.lastPointer = this.arraySize;
     }
 
-    private void resizeSmall(int new_Size) {
-        T[] resized_Items = (T []) new Object[new_Size];
+    private void resizeSmall(int newSize) {
+        T[] resizedItems = (T[]) new Object[newSize];
         int start = (this.firstPointer + 1) % this.size;
-        for (int i = 0; i < this.array_Size; i++) {
-            resized_Items[i] = this.items[start];
-            if (start == this.size - 1){
+        for (int i = 0; i < this.arraySize; i++) {
+            resizedItems[i] = this.items[start];
+            if (start == this.size - 1) {
                 start = 0;
-            }
-            else{
+            } else {
                 start += 1;
             }
         }
-        this.size = new_Size;
-        this.items = resized_Items;
+        this.size = newSize;
+        this.items = resizedItems;
         this.firstPointer = this.size - 1;
-        this.lastPointer = this.array_Size;
+        this.lastPointer = this.arraySize;
     }
 
     public T get(int index) {
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,11 +1,16 @@
 public class LinkedListDeque<T> {
+
     private Node sentinel;
     private int size;
-    /**Node class with prev, next, and generic item attribute*/
+
+    /**
+     * Node class with prev, next, and generic item attribute
+     */
     public class Node<T> {
         Node prev;
         Node next;
         T item;
+
         public Node(T i) {
             this.item = i;
         }
@@ -21,56 +26,59 @@ public class LinkedListDeque<T> {
     public LinkedListDeque(LinkedListDeque other) {
         this();
         for (int i = other.size - 1; i >= 0; i--) {
-            this.addFirst((T)other.get(i));
+            this.addFirst((T) other.get(i));
         }
 
 
     }
+
     public void addFirst(T item) {
-        Node new_Node = new Node(item);
-        sentinel.next.prev = new_Node;
-        new_Node.next = sentinel.next;
-        new_Node.prev = sentinel;
-        sentinel.next = new_Node;
+        Node newNode = new Node(item);
+        sentinel.next.prev = newNode;
+        newNode.next = sentinel.next;
+        newNode.prev = sentinel;
+        sentinel.next = newNode;
         size += 1;
     }
+
     public T removeFirst() {
-        T ret_Item = (T) sentinel.next.item;
+        T retItem = (T) sentinel.next.item;
         Node temp = sentinel.next;
         sentinel.next = sentinel.next.next;
         temp.next.prev = sentinel;
         if (size == 0) {
-            return ret_Item;
+            return retItem;
         }
         this.size -= 1;
-        return ret_Item;
+        return retItem;
     }
 
     public void addLast(T item) {
-        Node new_Node = new Node(item);
-        new_Node.next = sentinel;
-        new_Node.prev = sentinel.prev;
-        sentinel.prev.next = new_Node;
-        sentinel.prev = new_Node;
+        Node newNode = new Node(item);
+        newNode.next = sentinel;
+        newNode.prev = sentinel.prev;
+        sentinel.prev.next = newNode;
+        sentinel.prev = newNode;
         size += 1;
     }
 
     public T removeLast() {
-        T ret_Item = (T) sentinel.prev.item;
+        T retItem = (T) sentinel.prev.item;
         sentinel.prev = sentinel.prev.prev;
         sentinel.prev.next = sentinel;
         if (size == 0) {
-            return ret_Item;
+            return retItem;
         }
         this.size -= 1;
-        return ret_Item;
+        return retItem;
     }
 
     public int size() {
         return size;
     }
+
     public boolean isEmpty() {
-        return size ==0;
+        return size == 0;
     }
 
     public T get(int index) {
@@ -90,14 +98,13 @@ public class LinkedListDeque<T> {
     private T helper(int i, Node n) {
         if (i == 0) {
             return (T) n.item;
-        }
-        else {
+        } else {
             n = n.next;
             return helper(--i, n);
         }
     }
 
-    public void printDeque(){
+    public void printDeque() {
         String deque = "";
         Node node = sentinel.next;
         while (node.item != null) {
