cfd51ea4c7a0754dc5f6c84d4455d800513c764b
David Lu <davidlu@berkeley.edu>
2019-02-11 20:16:15 -0800

    ok!

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,6 +1,6 @@
 public class Palindrome {
     public Deque<Character> wordToDeque(String word){
-        LinkedListDeque<Character> testDeque = new LinkedListDeque<Character>();
+        Deque<Character> testDeque = new LinkedListDeque<Character>();
         for(char c: word.toCharArray()){
             testDeque.addLast(c);
         }
@@ -8,6 +8,18 @@ public class Palindrome {
 
     }
     public boolean isPalindrome(String word){
+        Palindrome palindrome = new Palindrome();
+        Deque palindromeDeque = palindrome.wordToDeque(word);
+        Deque palindromeDequeBack = palindrome.wordToDeque(word);
+        return helper(palindromeDeque, palindromeDequeBack);
+    }
+    private boolean helper(Deque p1, Deque p2){
+        if (p1.size() == 0 || p2.size()==0){
+            return true;
+        }
+        else if(p1.removeFirst() == p2.removeLast()){
+            return helper(p1, p2);
+        }
         return false;
     }
 }
