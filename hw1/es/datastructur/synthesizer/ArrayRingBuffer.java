836905341ad845e9e5b9bf35901f65ab33580a4b
David Lu <davidlu@berkeley.edu>
2019-02-26 00:43:55 -0800

    m

--- hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
+++ hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
@@ -2,9 +2,6 @@ package es.datastructur.synthesizer;
 
 import java.util.Iterator;
 
-//TODO: Make sure to that this class and all of its methods are public
-//TODO: Make sure to add the override tag for all overridden methods
-//TODO: Make sure to make this class implement BoundedQueue<T>
 
 public class ArrayRingBuffer<T> implements BoundedQueue<T> {
     /* Index for the next dequeue or peek. */
@@ -110,7 +107,4 @@ public class ArrayRingBuffer<T> implements BoundedQueue<T> {
             return true;
         }
     }
-    // TODO: When you get to part 4, implement the needed code to support
-    //       iteration and equals.
 }
-// TODO: Remove all comments that say TODO when you're done.
--- hw1/es/datastructur/synthesizer/GuitarString.java
+++ hw1/es/datastructur/synthesizer/GuitarString.java
@@ -17,10 +17,6 @@ public class GuitarString {
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
-        // TODO: Create a buffer with capacity = SR / frequency. You'll need to
-        //       cast the result of this division operation into an int. For
-        //       better accuracy, use the Math.round() function before casting.
-        //       Your buffer should be initially filled with zeros.
         int result = (int) Math.round(SR / frequency);
         buffer = new ArrayRingBuffer<Double>(result);
         for (int i = 0; i < buffer.capacity(); i++) {
@@ -31,12 +27,6 @@ public class GuitarString {
 
     /* Pluck the guitar string by replacing the buffer with white noise. */
     public void pluck() {
-        // TODO: Dequeue everything in buffer, and replace with random numbers
-        //       between -0.5 and 0.5. You can get such a number by using:
-        //       double r = Math.random() - 0.5;
-        //
-        //       Make sure that your random numbers are different from each
-        //       other.
         double r = Math.random() - 0.5;
         for (int i = 0; i < buffer.capacity(); i++) {
             buffer.dequeue();
@@ -51,9 +41,6 @@ public class GuitarString {
      * the Karplus-Strong algorithm.
      */
     public void tic() {
-        // TODO: Dequeue the front sample and enqueue a new sample that is
-        //       the average of the two multiplied by the DECAY factor.
-        //       Do not call StdAudio.play().
         double first = buffer.dequeue();
         double next = buffer.peek();
         double newDouble = DECAY * (first + next) / 2;
@@ -65,4 +52,3 @@ public class GuitarString {
         return buffer.peek();
     }
 }
-// TODO: Remove all comments that say TODO when you're done.
