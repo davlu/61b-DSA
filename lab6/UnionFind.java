f2102c018baaceb60026bf94189e3c06eaab5d51
David Lu <davidlu@berkeley.edu>
2019-02-28 17:05:00 -0800

    pls

--- lab6/UnionFind.java
+++ lab6/UnionFind.java
@@ -1,34 +1,55 @@
 public class UnionFind {
 
     // TODO - Add instance variables?
-
+    int[] set;
     /* Creates a UnionFind data structure holding n vertices. Initially, all
        vertices are in disjoint sets. */
     public UnionFind(int n) {
-        // TODO
+        set = new int[n];
+        for(int i =0 ; i<n-1; i++){
+            set[n] = -1;
+        }
     }
 
     /* Throws an exception if v1 is not a valid index. */
     private void validate(int vertex) {
-        // TODO
+        boolean validated = vertex <= set.length-1 && vertex >= 0;
+        if(!validated){
+            throw new IllegalArgumentException("v1 is not a valid index");
+        }
+        else{
+            return;
+        }
     }
 
     /* Returns the size of the set v1 belongs to. */
     public int sizeOf(int v1) {
-        // TODO
+        validate(v1);
+        int size = 0;
+        for(int i = 0; i < set.length; i++){
+            if(set[i] == set[v1]){
+                size ++;
+            }
+        }
         return -1;
     }
 
     /* Returns the parent of v1. If v1 is the root of a tree, returns the
        negative size of the tree for which v1 is the root. */
     public int parent(int v1) {
-        // TODO
+        validate(v1);
+        int parent = set[v1];
+        if(parent != -1){
+            return parent;
+        }
         return -1;
     }
 
     /* Returns true if nodes v1 and v2 are connected. */
     public boolean connected(int v1, int v2) {
-        // TODO
+        if(find(v1)==find(v2)){
+            return true;
+        }
         return false;
     }
 
@@ -44,8 +65,14 @@ public class UnionFind {
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. */
     public int find(int vertex) {
-        // TODO
-        return -1;
+        validate(vertex);
+        int rootVal = set[vertex];
+        if(rootVal == -1){
+            return rootVal;
+        }
+        else{
+            return find(set[rootVal]);
+        }
     }
 
 }
