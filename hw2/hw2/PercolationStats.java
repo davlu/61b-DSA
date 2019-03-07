e17b4b614a7b5ec4ee9fe6c9f1467eca2460d99b
David Lu <davidlu@berkeley.edu>
2019-03-06 20:23:35 -0800

    PL0X

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,23 +1,45 @@
 package hw2;
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+
 
 public class PercolationStats {
+    double[] values;
     public PercolationStats(int N, int T, PercolationFactory pf) {
+        if(N <= 0 || T <= 0){
+            throw new java.lang.IllegalArgumentException();
+        }
+        double[] values = new double[T];
+        for(int i = 0; i<T; i++){
+            Percolation newPercolation = pf.make(N);
+            boolean percolated = false;
+            while(!percolated){
+                newPercolation.open(StdRandom.uniform(0,N), StdRandom.uniform(0,N));
+                percolated = newPercolation.percolates();
+            }
+            int openSites = newPercolation.numberOfOpenSites();
+            values[i] = (double)openSites/(N*N);
+        }
     }// perform T independent experiments on an N-by-N grid
 
     public double mean() {
-        return 0.0;
+        double sum = 0;
+        for(int i =0 ; i < values.length; i++){
+            sum+=values[i];
+        }
+        return sum/values.length;
     }// sample mean of percolation threshold
 
     public double stddev() {
-        return 0.0;
+        return StdStats.stddev(this.values);
     }// sample standard deviation of percolation threshold
 
     public double confidenceLow() {
-        return 0.0;
+        return this.mean()-(1.96*values.length)/(Math.pow(values.length, 0.5));
     }// low endpoint of 95% confidence interval
 
     public double confidenceHigh() {
-        return 0.0;
+        return this.mean()+(1.96*values.length)/(Math.pow(values.length, 0.5));
     }// high endpoint of 95% confidence interval
 }
 
