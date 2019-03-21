1109b6540dca68847adf0742601a3404083603c3
David Lu <davidlu@berkeley.edu>
2019-03-21 02:22:06 -0700

    fini

--- proj2ab/bearmaps/KDTest.java
+++ proj2ab/bearmaps/KDTest.java
@@ -3,25 +3,95 @@ package bearmaps;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+
 import java.util.List;
+import java.util.ArrayList;
 
 public class KDTest {
     @Test
-    public void sanityCheckTest(){
-        Point p1 = new Point(2,3);
-        Point p2 = new Point(4,2);
-        Point p3 = new Point(4,2);
-        Point p4 = new Point(4,5);
+    public void sanityCheckTest() {
+        Point p1 = new Point(2, 3);
+        Point p2 = new Point(4, 2);
+        Point p3 = new Point(4, 2);
+        Point p4 = new Point(4, 5);
         Point p5 = new Point(3, 3);
         Point p6 = new Point(1, 5);
         Point p7 = new Point(4, 4);
-        KDTree nn = new KDTree(List.of(p1, p2, p3,p4,p5,p6,p7));
+        KDTree nn = new KDTree(List.of(p1, p2, p3, p4, p5, p6, p7));
         /**
-        Point ret = nn.nearest(3.0, 4.0); // returns p2
-        assertEquals(new Point(3.3, 4.4), ret);
-        assertEquals(3.3, ret.getX(), 0.01); // evaluates to 3.3
-        assertEquals(4.4, ret.getY(), 0.01); // evaluates to 4.4
+         Point ret = nn.nearest(3.0, 4.0); // returns p2
+         assertEquals(new Point(3.3, 4.4), ret);
+         assertEquals(3.3, ret.getX(), 0.01); // evaluates to 3.3
+         assertEquals(4.4, ret.getY(), 0.01); // evaluates to 4.4
          **/
+    }
+
+    @Test
+    public void testNearest() {
+        Point p1 = new Point(2, 3);
+        Point p2 = new Point(4, 2);
+        Point p3 = new Point(4, 2);
+        Point p4 = new Point(4, 5);
+        Point p5 = new Point(3, 3);
+        Point p6 = new Point(1, 5);
+        Point p7 = new Point(4, 4);
+        KDTree nn = new KDTree(List.of(p1, p2, p3, p4, p5, p6, p7));
+        Point nearest = nn.nearest(0, 7);
+        assertEquals(nearest, new Point(1, 5));
+    }
+
+    @Test
+    public void hugeNearest() {
+        List<Point> pointList = new ArrayList<>();
+        List<Point> findPoints = new ArrayList<>();
+        int rounds = 100000;
+        for (int i = 0; i < rounds; i++) {                  //generate test values
+            double randPointX = Math.random() * 1000;
+            double randPointY = Math.random() * 1000;
+            pointList.add(new Point(randPointX, randPointY));
+        }
+        for (int i = 0; i < 10000; i++) {
+            double randPointX = Math.random() * 1000;
+            double randPointY = Math.random() * 1000;
+            findPoints.add(new Point(randPointX, randPointY));
+        }
+        NaivePointSet naive = new NaivePointSet(pointList);
+        KDTree kdTree = new KDTree(pointList);
+        for (Point p : findPoints) {
+            assertEquals(naive.nearest(p.getX(), p.getY()), kdTree.nearest(p.getX(), p.getY()));
+        }
+    }
+
+    @Test
+    public void timeTest() {
+        List<Point> pointList = new ArrayList<>();
+        List<Point> findPoints = new ArrayList<>();
+        int rounds = 100000;
+        for (int i = 0; i < rounds; i++) {                  //generate test values
+            double randPointX = Math.random() * 1000;
+            double randPointY = Math.random() * 1000;
+            pointList.add(new Point(randPointX, randPointY));
+        }
+        for (int i = 0; i < 10000; i++) {
+            double randPointX = Math.random() * 1000;
+            double randPointY = Math.random() * 1000;
+            findPoints.add(new Point(randPointX, randPointY));
+        }
+        NaivePointSet naive = new NaivePointSet(pointList);
+        KDTree kdTree = new KDTree(pointList);
+
+        int startTime = (int) System.currentTimeMillis();
+        for (int i = 0; i < findPoints.size(); i++) {
+            naive.nearest(findPoints.get(i).getX(), findPoints.get(i).getY());
+        }
+        int difference = (int) System.currentTimeMillis() - startTime;
+        System.out.println("Time taken for naive heap implementation: " + difference);
+
+        int startTime1 = (int) System.currentTimeMillis();
+        for (int i = 0; i < findPoints.size(); i++) {
+            kdTree.nearest(findPoints.get(i).getX(), findPoints.get(i).getY());
+        }
+        int difference1 = (int) System.currentTimeMillis() - startTime1;
+        System.out.println("Time taken for your heap implementation: " + difference1);
     }
 }
--- proj2ab/bearmaps/KDTree.java
+++ proj2ab/bearmaps/KDTree.java
@@ -1,106 +1,119 @@
 package bearmaps;
+
 import java.util.List;
 
 public class KDTree {
     private Node root;
     private List<Point> listPoints;
-    public KDTree(List<Point> points){
+
+    public KDTree(List<Point> points) {
         this.listPoints = points;
         Node firstNode = new Node(listPoints.get(0), null, null);
         this.root = firstNode;
-        for(int i =1; i<listPoints.size(); i++){
-            Node newNode = new Node(listPoints.get(i), null, null );
+        for (int i = 1; i < listPoints.size(); i++) {
+            Node newNode = new Node(listPoints.get(i), null, null);
             this.root.add(newNode);
             newNode.orientation = !(newNode.orientation);
         }
     }
-    private static class Node{
+
+    private static class Node implements Comparable<Point> {
         private Point p;
         private boolean orientation = true; //false is vertical y, true is horizontal x
         Node greater;
         Node lesser;
-        public Node(Point point, Node greater, Node lesser){
+
+        Node(Point point, Node greater, Node lesser) {
             this.p = point;
             this.greater = greater;
             this.lesser = lesser;
         }
-        public void add(Node n){
-            if(orientation == true){
-                if((this.p.getX() == n.p.getX()) && (this.p.getY() == n.p.getY())){
+
+        public int compareTo(Point p1) {
+            if (this.orientation) {
+                return (int) (this.p.getX() - p1.getX());
+            }
+            return (int) (this.p.getY() - p1.getY());
+        }
+
+        public void add(Node n) {
+            if (orientation) {
+                if ((this.p.getX() == n.p.getX()) && (this.p.getY() == n.p.getY())) {
                     return;
-                }
-                else if(this.p.getX() > n.p.getX()){
-                    if(this.lesser != null){
+                } else if (this.p.getX() > n.p.getX()) {
+                    if (this.lesser != null) {
                         n.orientation = !n.orientation;
                         this.lesser.add(n);
-                    }
-                    else{
+                    } else {
                         this.lesser = n;
                     }
-                }
-                else{
-                    if(this.greater != null){
+                } else {
+                    if (this.greater != null) {
                         n.orientation = !n.orientation;
                         this.greater.add(n);
+                    } else {
+                        this.greater = n;
                     }
-                    else{
+                }
+            } else {
+                if ((this.p.getX() == n.p.getX()) && (this.p.getY() == n.p.getY())) {
+                    return;
+                } else if (this.p.getY() > n.p.getY()) {
+                    if (this.lesser != null) {
+                        n.orientation = !n.orientation;
+                        this.lesser.add(n);
+                    } else {
+                        this.lesser = n;
+                    }
+                } else {
+                    if (this.greater != null) {
+                        n.orientation = !n.orientation;
+                        this.greater.add(n);
+                    } else {
                         this.greater = n;
                     }
                 }
             }
-            else{
-                if((this.p.getX() == n.p.getX()) && (this.p.getY() == n.p.getY())){
-                    return;
-                }
-                else if(this.p.getY() > n.p.getY()){
-                    if(this.lesser != null){
-                        n.orientation = !n.orientation;
-                        this.lesser.add(n);
-                    }
-                    else{
-                        this.lesser = n;
-                    }
-                }
-                else{
-                    if(this.greater != null){
-                        n.orientation = !n.orientation;
-                        this.greater.add(n);
-                    }
-                    else{
-                        this.greater = n;
-                    }
-                }
+        }
+    }
+
+    public Point nearest(double x, double y) {
+        Point pointOfInterest = new Point(x, y);
+        return helperFunction(pointOfInterest, this.root, this.root).p;
+    }
+
+    private Node helperFunction(Point pointOfInterest, Node currentNode, Node bestNode) {
+        if (currentNode == null) {
+            return bestNode;
+        }
+        Double newDistance = Point.distance(pointOfInterest, currentNode.p);
+        Double shortestD = Point.distance(pointOfInterest, bestNode.p);
+        if (newDistance < shortestD) {
+            bestNode = currentNode;
+        }
+        if (currentNode.compareTo(pointOfInterest) < 0) {
+            Node best = helperFunction(pointOfInterest, currentNode.greater, bestNode);
+            if (currentNode.lesser != null
+                    && bestPossibleDistance(pointOfInterest, currentNode.lesser)
+                    < Point.distance(best.p, pointOfInterest)) {
+                best = helperFunction(pointOfInterest, currentNode.lesser, best);
             }
+            return best;
+        } else {
+            Node best = helperFunction(pointOfInterest, currentNode.lesser, bestNode);
+            if (currentNode.greater != null
+                    && bestPossibleDistance(pointOfInterest, currentNode.greater)
+                    < Point.distance(best.p, pointOfInterest)) {
+                best = helperFunction(pointOfInterest, currentNode.greater, best);
+            }
+            return best;
         }
-    }
-    public Point nearest(double x, double y){
-        Point pointOfInterest = new Point(x,y);
-        return helperFunction(pointOfInterest, this.root, 100000.0);
     }
 
-    private Point helperFunction(Point pointOfInterest, Node root, Double shortestDist){
-        if(root.lesser == null && root.greater == null){
-            return root.p;
+    private double bestPossibleDistance(Point pointOfInterest, Node otherPoint) {
+        if (!otherPoint.orientation) {
+            return Math.pow(Math.abs(pointOfInterest.getX() - otherPoint.p.getX()), 2);
         }
-        Double shorter = shortestDist;
-        if(Point.distance(root.p, pointOfInterest) < shortestDist){
-            shorter = Point.distance(root.p, pointOfInterest);
-        }
-        if(root.lesser.p!=null && distX(pointOfInterest, root.lesser.p) < distX(pointOfInterest, root.greater.p)){
-            return helperFunction(pointOfInterest, root.lesser, shorter);
-        }
-    }
-    private double bestPossibleDistance(Point pointOfInterest, Node otherPoint){ //looks on the infinite line for best
-        if(otherPoint.orientation == false){
-            return Math.abs(pointOfInterest.getX()-otherPoint.p.getX());
-        }
-        return Math.abs(pointOfInterest.getY()-otherPoint.p.getY());
-    }
-
-    private double distX(Point pointOfInterest, Point otherPoint){
-        return Math.abs(pointOfInterest.getX() - otherPoint.getX());
-    }
-    private double distY(Point pointOfInterest, Point otherPoint){
-        return Math.abs(pointOfInterest.getY() - otherPoint.getY());
+        return Math.pow(Math.abs(pointOfInterest.getY() - otherPoint.p.getY()), 2);
     }
 }
