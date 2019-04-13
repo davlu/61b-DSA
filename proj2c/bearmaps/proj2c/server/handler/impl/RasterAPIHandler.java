fc514a1832b7fb695617545870e0ef8bc99e2831
David Lu <davidlu@berkeley.edu>
2019-04-12 23:47:29 -0700

    added part 1

--- proj2c/bearmaps/proj2c/server/handler/impl/RasterAPIHandler.java
+++ proj2c/bearmaps/proj2c/server/handler/impl/RasterAPIHandler.java
@@ -17,12 +17,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static bearmaps.proj2c.utils.Constants.SEMANTIC_STREET_GRAPH;
-import static bearmaps.proj2c.utils.Constants.ROUTE_LIST;
+import static bearmaps.proj2c.utils.Constants.*;
 
 /**
  * Handles requests from the web browser for map images. These images
  * will be rastered into one large image to be displayed to the user.
+ *
  * @author rahul, Josh Hug, _________
  */
 public class RasterAPIHandler extends APIRouteHandler<Map<String, Double>, Map<String, Object>> {
@@ -54,22 +54,21 @@ public class RasterAPIHandler extends APIRouteHandler<Map<String, Double>, Map<S
     /**
      * Takes a user query and finds the grid of images that best matches the query. These
      * images will be combined into one big image (rastered) by the front end. <br>
-     *
-     *     The grid of images must obey the following properties, where image in the
-     *     grid is referred to as a "tile".
-     *     <ul>
-     *         <li>The tiles collected must cover the most longitudinal distance per pixel
-     *         (LonDPP) possible, while still covering less than or equal to the amount of
-     *         longitudinal distance per pixel in the query box for the user viewport size. </li>
-     *         <li>Contains all tiles that intersect the query bounding box that fulfill the
-     *         above condition.</li>
-     *         <li>The tiles must be arranged in-order to reconstruct the full image.</li>
-     *     </ul>
+     * <p>
+     * The grid of images must obey the following properties, where image in the
+     * grid is referred to as a "tile".
+     * <ul>
+     * <li>The tiles collected must cover the most longitudinal distance per pixel
+     * (LonDPP) possible, while still covering less than or equal to the amount of
+     * longitudinal distance per pixel in the query box for the user viewport size. </li>
+     * <li>Contains all tiles that intersect the query bounding box that fulfill the
+     * above condition.</li>
+     * <li>The tiles must be arranged in-order to reconstruct the full image.</li>
+     * </ul>
      *
      * @param requestParams Map of the HTTP GET request's query parameters - the query box and
-     *               the user viewport width and height.
-     *
-     * @param response : Not used by this function. You may ignore.
+     *                      the user viewport width and height.
+     * @param response      : Not used by this function. You may ignore.
      * @return A map of results for the front end as specified: <br>
      * "render_grid"   : String[][], the files to display. <br>
      * "raster_ul_lon" : Number, the bounding upper left longitude of the rastered image. <br>
@@ -77,18 +76,51 @@ public class RasterAPIHandler extends APIRouteHandler<Map<String, Double>, Map<S
      * "raster_lr_lon" : Number, the bounding lower right longitude of the rastered image. <br>
      * "raster_lr_lat" : Number, the bounding lower right latitude of the rastered image. <br>
      * "depth"         : Number, the depth of the nodes of the rastered image;
-     *                    can also be interpreted as the length of the numbers in the image
-     *                    string. <br>
+     * can also be interpreted as the length of the numbers in the image
+     * string. <br>
      * "query_success" : Boolean, whether the query was able to successfully complete; don't
-     *                    forget to set this to true on success! <br>
+     * forget to set this to true on success! <br>
      */
     @Override
     public Map<String, Object> processRequest(Map<String, Double> requestParams, Response response) {
-        //System.out.println("yo, wanna know the parameters given by the web browser? They are:");
-        //System.out.println(requestParams);
+        double deltaLongD0 = ROOT_LRLON - ROOT_ULLON;
+        double D0LDPP = deltaLongD0 / TILE_SIZE;
+        if((requestParams.get("lrlon")<ROOT_ULLON) || (requestParams.get("ullon")>ROOT_LRLON)
+            || (requestParams.get("lrlat") > ROOT_ULLAT) || (requestParams.get("ullat") < ROOT_ULLAT)){
+            return queryFail();
+        }
+        double queryBoxDeltaLong = requestParams.get("lrlon") - requestParams.get("ullon");
+        double queryBoxLDPP = queryBoxDeltaLong / TILE_SIZE;
+        int bestD = 0;
+        for (double i = 0.0; i < 8.0; i++) {                //find the bestD resolution for queryBOX.
+            double testLDPP = D0LDPP / Math.pow(2.0, i);
+            if (testLDPP <= queryBoxLDPP) {
+                bestD = (int) i;
+            }
+        }
+        double newBoxWandH = deltaLongD0 / (Math.pow(2.0, (double) bestD));
+        double numBoxFromLEdgeToQBOXL = Math.ceil((requestParams.get("ullon") - ROOT_ULLON) / newBoxWandH);
+        double numBoxFromLEdgeToQBOXR = Math.ceil((requestParams.get("lrlon") - ROOT_ULLON) / newBoxWandH);
+        double numBoxFromTopToQBOXU = Math.ceil((ROOT_ULLAT - requestParams.get("ullat")) / newBoxWandH);
+        double numBoxFromTopToQBOXL = Math.ceil((ROOT_ULLAT - requestParams.get("lrlat")) / newBoxWandH);
+        double numBoxesX = numBoxFromLEdgeToQBOXR - numBoxFromLEdgeToQBOXL;
+        double numBoxesY = numBoxFromTopToQBOXL - numBoxFromTopToQBOXU;
+
+        String[][] nestedArrayOfPNG = new String[(int)numBoxesX][(int)numBoxesY];
+        for(int i = 0; i<numBoxesY; i++){
+            for(int j = 0; j<numBoxesX; j++){
+                nestedArrayOfPNG[i][j] = "d"+bestD+"_x"+numBoxFromLEdgeToQBOXL+j+"_y"+numBoxFromTopToQBOXU+i+".png";
+            }
+        }
+
         Map<String, Object> results = new HashMap<>();
-        System.out.println("Since you haven't implemented RasterAPIHandler.processRequest, nothing is displayed in "
-                + "your browser.");
+        results.put("render_grid", results);
+        results.put("raster_ul_lon", (numBoxFromLEdgeToQBOXL*newBoxWandH)+ROOT_ULLON);
+        results.put("raster_ul_lat", (numBoxFromTopToQBOXU*newBoxWandH)+ROOT_ULLAT);
+        results.put("raster_lr_lon", (numBoxFromLEdgeToQBOXR*newBoxWandH)+ROOT_ULLON);
+        results.put("raster_lr_lat", (numBoxFromTopToQBOXL*newBoxWandH)+ROOT_ULLAT);
+        results.put("depth", bestD);
+        results.put("query_success", true);
         return results;
     }
 
@@ -119,6 +151,7 @@ public class RasterAPIHandler extends APIRouteHandler<Map<String, Double>, Map<S
 
     /**
      * Validates that Rasterer has returned a result that can be rendered.
+     *
      * @param rip : Parameters provided by the rasterer
      */
     private boolean validateRasteredImgParams(Map<String, Object> rip) {
@@ -143,8 +176,8 @@ public class RasterAPIHandler extends APIRouteHandler<Map<String, Double>, Map<S
      * In Spring 2016, students had to do this on their own, but in 2017,
      * we made this into provided code since it was just a bit too low level.
      */
-    private  void writeImagesToOutputStream(Map<String, Object> rasteredImageParams,
-                                                  ByteArrayOutputStream os) {
+    private void writeImagesToOutputStream(Map<String, Object> rasteredImageParams,
+                                           ByteArrayOutputStream os) {
         String[][] renderGrid = (String[][]) rasteredImageParams.get("render_grid");
         int numVertTiles = renderGrid.length;
         int numHorizTiles = renderGrid[0].length;
