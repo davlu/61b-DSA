4de8fc7a1583c7a479232262016d3ab9970d273b
David Lu <davidlu@berkeley.edu>
2019-04-14 23:44:00 -0700

    added part  II

--- proj2c/bearmaps/proj2c/AugmentedStreetMapGraph.java
+++ proj2c/bearmaps/proj2c/AugmentedStreetMapGraph.java
@@ -30,12 +30,16 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
      * @return The id of the node in the graph closest to the target.
      */
     public long closest(double lon, double lat) {
+        List<Node> nodes = this.getNodes();
         List<Point> points = new ArrayList<>();
         Map<Point, Node> pointToNode= new HashMap<>();
         for(Node n : nodes){
             double calculatedY = n.lat();
             double calculatedX = n.lon();
             Point convertedPoint = new Point(calculatedX, calculatedY);
+            if(this.neighbors(n.id()).size()==0){
+                continue;
+            }
             pointToNode.put(convertedPoint, n);
             points.add(convertedPoint);
         }
