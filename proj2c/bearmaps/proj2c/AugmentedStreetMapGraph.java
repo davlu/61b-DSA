0e203935428a8d2e2e297c905f00945b092cfeb6
David Lu <davidlu@berkeley.edu>
2019-04-17 05:24:37 -0700

    tf

--- proj2c/bearmaps/proj2c/AugmentedStreetMapGraph.java
+++ proj2c/bearmaps/proj2c/AugmentedStreetMapGraph.java
@@ -67,6 +67,37 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
 
     /**
      * For Project Part III (gold points)
+     * Collect all locations that match a cleaned <code>locationName</code>, and return
+     * information about each node that matches.
+     *
+     * @param locationName A full name of a location searched for.
+     * @return A list of locations whose cleaned name matches the
+     * cleaned <code>locationName</code>, and each location is a map of parameters for the Json
+     * response as specified: <br>
+     * "lat" -> Number, The latitude of the node. <br>
+     * "lon" -> Number, The longitude of the node. <br>
+     * "name" -> String, The actual name of the node. <br>
+     * "id" -> Number, The id of the node. <br>
+     */
+    public List<Map<String, Node>> getLocations(String locationName) {
+        List<Map<String, Node>> result = new LinkedList<>();
+        if(locationName!= null){
+            String cleaned = cleanString(locationName);
+            for(Node n : stringToNode.get(cleaned)){
+                if(n.name() != null){
+                    if (cleanString(n.name()).equals(cleaned)) {
+                        Map<String, Node> newMap = new HashMap<>();
+                        newMap.put(cleaned, n);
+                        result.add(newMap);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * For Project Part III (gold points)
      * In linear time, collect all the names of OSM locations that prefix-match the query string.
      *
      * @param prefix Prefix string to be searched for. Could be any case, with our without
@@ -89,37 +120,6 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
             }
         }
         return results;
-    }
-
-    /**
-     * For Project Part III (gold points)
-     * Collect all locations that match a cleaned <code>locationName</code>, and return
-     * information about each node that matches.
-     *
-     * @param locationName A full name of a location searched for.
-     * @return A list of locations whose cleaned name matches the
-     * cleaned <code>locationName</code>, and each location is a map of parameters for the Json
-     * response as specified: <br>
-     * "lat" -> Number, The latitude of the node. <br>
-     * "lon" -> Number, The longitude of the node. <br>
-     * "name" -> String, The actual name of the node. <br>
-     * "id" -> Number, The id of the node. <br>
-     */
-    public List<Map<String, Object>> getLocations(String locationName) {
-        List<Map<String, Object>> result = new LinkedList<>();
-        if(locationName!= null){
-            String cleaned = cleanString(locationName);
-            for(Node n : stringToNode.get(cleaned)){
-                if(n.name() != null){
-                    if (cleanString(n.name()).equals(cleaned)) {
-                        Map<String, Object> newMap = new HashMap<>();
-                        newMap.put(cleaned, n);
-                        result.add(newMap);
-                    }
-                }
-            }
-        }
-        return result;
     }
 
     /**
