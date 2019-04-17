2f703dcf6fb66cc63baeae9c8e6f4fb000d5b7a5
David Lu <davidlu@berkeley.edu>
2019-04-16 23:51:56 -0700

    bug

--- proj2c/bearmaps/proj2c/AugmentedStreetMapGraph.java
+++ proj2c/bearmaps/proj2c/AugmentedStreetMapGraph.java
@@ -12,7 +12,6 @@ import java.util.*;
  * An augmented graph that is more powerful that a standard StreetMapGraph.
  * Specifically, it supports the following additional operations:
  *
- *
  * @author Alan Yao, Josh Hug, ________
  */
 public class AugmentedStreetMapGraph extends StreetMapGraph {
@@ -21,22 +20,23 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
     private Map<Point, Node> pointToNode;
     private TrieSet trie;
     private Map<String, LinkedList<String>> cleanedToReal;
+
     public AugmentedStreetMapGraph(String dbPath) {
         super(dbPath);
         nodes = this.getNodes();
         points = new ArrayList<>();
-        pointToNode= new HashMap<>();
+        pointToNode = new HashMap<>();
         trie = new TrieSet();
         cleanedToReal = new HashMap<>();
 
-        for(Node n : nodes){
+        for (Node n : nodes) {
             double calculatedY = n.lat();
             double calculatedX = n.lon();
             Point convertedPoint = new Point(calculatedX, calculatedY);
-            if(n.name() != null) {
+            if (n.name() != null) {
                 String cleaned = cleanString(n.name());
                 trie.add(cleaned);
-                if(cleaned == ""){
+                if (cleaned == "") {
                     cleaned = "0";
                 }
                 if (cleanedToReal.containsKey(cleaned)) {
@@ -47,16 +47,18 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
                     cleanedToReal.put(cleaned, newBucket);
                 }
             }
-            if(this.neighbors(n.id()).size()==0){
+            if (this.neighbors(n.id()).size() == 0) {
                 continue;
             }
             pointToNode.put(convertedPoint, n);
             points.add(convertedPoint);
         }
     }
+
     /**
      * For Project Part III (gold points)
      * In linear time, collect all the names of OSM locations that prefix-match the query string.
+     *
      * @param prefix Prefix string to be searched for. Could be any case, with our without
      *               punctuation.
      * @return A <code>List</code> of the full names of locations whose cleaned name matches the
@@ -65,17 +67,15 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
     public List<String> getLocationsByPrefix(String prefix) {
         String cleaned = cleanString(prefix);
         List<String> results = new LinkedList<>();
-        if(cleaned == ""){
+        if (cleaned == "") {
             cleaned = "0";
-            for(String p : cleanedToReal.get(cleaned)){
+            for (String p : cleanedToReal.get(cleaned)) {
                 results.add(p);
             }
         }
-        if(cleanedToReal.containsKey(cleaned)){
-            for(String s : trie.keysWithPrefix(prefix)){
-                for(String p: cleanedToReal.get(s)){
-                    results.add(p);
-                }
+        for (String s : trie.keysWithPrefix(prefix)) {
+            for (String p : cleanedToReal.get(s)) {
+                results.add(p);
             }
         }
         return results;
@@ -85,6 +85,7 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
      * For Project Part III (gold points)
      * Collect all locations that match a cleaned <code>locationName</code>, and return
      * information about each node that matches.
+     *
      * @param locationName A full name of a location searched for.
      * @return A list of locations whose cleaned name matches the
      * cleaned <code>locationName</code>, and each location is a map of parameters for the Json
@@ -101,6 +102,7 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
     /**
      * Useful for Part III. Do not modify.
      * Helper to process strings into their "cleaned" form, ignoring punctuation and capitalization.
+     *
      * @param s Input string.
      * @return Cleaned string.
      */
@@ -112,13 +114,14 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
     /**
      * For Project Part II
      * Returns the vertex closest to the given longitude and latitude.
+     *
      * @param lon The target longitude.
      * @param lat The target latitude.
      * @return The id of the node in the graph closest to the target.
      */
     public long closest(double lon, double lat) {
         WeirdPointSet kd = new WeirdPointSet(this.points);
-        Point best = kd.nearest(lon,lat);
+        Point best = kd.nearest(lon, lat);
         return pointToNode.get(best).id();
     }
 }
