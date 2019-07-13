d30d35bef22d80766270d608daee03a97154cbfd
David Lu <davidlu@berkeley.edu>
2019-07-13 16:21:58 -0700

    remove lab3 useless folders

--- lab3/Flik/FlikTest.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.Assert.*;
-import org.junit.Test;
-public class FlikTest {
-    @Test
-    public void testEquivalence() {
-        boolean b = Flik.isSameNumber(128,128);
-        assertTrue(Flik.isSameNumber(128, 128));
-    }
-}
