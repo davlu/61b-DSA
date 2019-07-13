f577834a44965a657126ddb8b55211c3a4c232db
David Lu <davidlu@berkeley.edu>
2019-07-13 16:06:50 -0700

    projects and labs

---
+++ test/src/BList.java
@@ -0,0 +1,9 @@
+public class BList<item> extends AList<item> {
+    public void penis(){
+        System.out.println("penis");
+    }
+    public static void main(String[] args){
+        BList<String> a1 = (BList) new AList<>();
+     //uses dynamic type AList's print method
+    }
+}
---
+++ test/src/List.java
@@ -0,0 +1,5 @@
+public interface List<item> {
+    default public void print(){
+        System.out.println("pener");
+    }
+}
